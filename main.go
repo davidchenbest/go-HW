ab88b39ef4a39e71ef79243b3c5df2346b98ba25
davidchenbest <davidchen108@gmail.com>
2023-07-08 14:26:36 -0400

    slice

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import(
-
+  "fmt"
 )
 
 func main()  {
+  names := []string{"susan","david"}
+  names = append(names,"gina")
+  l:=len(names)
+  fmt.Println(l)
+  fmt.Println(names[1:])
 }
