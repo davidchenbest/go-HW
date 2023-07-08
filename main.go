fa1de6cef64ebdcc4af653cbd865fa83ffb28ec1
davidchenbest <davidchen108@gmail.com>
2023-07-08 17:57:45 -0400

    map

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
 import(
-
+"fmt"
 )
 
 func main()  {
+  //initialize
+  // var names map[string]string
+  // var names = make(map[string]string)
+  var names = map[string]string {
+    "0": "John",
+    "1": "Jane",   // last comma is a must
+}
+
+  names["david"] = "david"
+
+  name,exist := names["david"]
+
+  delete(names, "1")
+
+  fmt.Println(name,exist)
+
+  for _, name := range names {
+    fmt.Println(name)
+  }
+
+
 }
