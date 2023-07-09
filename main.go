813f2ab373f19f411f4d116541669eefe5e6fbae
davidchenbest <davidchen108@gmail.com>
2023-07-09 16:37:44 -0400

    concurrency

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
 import(
+  "time"
+  "fmt"
+  "sync"
 
 )
 
+var wg sync.WaitGroup
+
 func main()  {
+  wg.Add(1)
+  Announce("hey", 1)
+  wg.Wait()
+}
+
+func Announce(message string, delay time.Duration) {
+  go func() {
+      time.Sleep(delay)
+      fmt.Println(message)
+      wg.Done()
+  }()  // Note the parentheses - must call the function.
 }
