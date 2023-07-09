1600805f5e6d6f16400a8ff8514d9357f1f41a4b
davidchenbest <davidchen108@gmail.com>
2023-07-09 16:09:52 -0400

    struct

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
 import(
-
+"fmt"
 )
 
+type User struct{
+  Name string
+  Age int
+}
+
 func main()  {
+  david := User{
+    Name:"david",
+    Age : 2,
+  }
+
+  users := []User{
+    david,
+  }
+
+  var p *User
+  p = &david
+
+  p.Name = "s"
+  p.Age =4
+
+  fmt.Println(p, david,users)
 }
