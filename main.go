71d9af40b4eb5b42cb7acbe4e2d477fb5f5ee6dc
davidchenbest <davidchen108@gmail.com>
2023-07-09 20:23:00 -0400

    interface

--- main.go
+++ main.go
@@ -1,8 +1,50 @@
 package main
 
 import(
-
+"fmt"
+"math"
 )
 
+type rect struct {
+  width, height float64
+}
+
+func (r rect) area() float64 {
+  return r.width * r.height
+}
+
+type cir struct {
+  radius float64
+}
+
+func (c cir) area() float64 {
+  return  math.Pi * c.radius * c.radius
+}
+
+type Shape interface{
+  area() float64
+}
+
+func area(shape Shape) float64{
+  return shape.area()
+}
+
 func main()  {
+
+  rectangle := rect{
+    width:2,
+    height:3,
+  }
+
+  circle := cir{
+    radius:2,
+  }
+  fmt.Println(area(rectangle),area(circle))
+
+  //EMPTY INTERFACE
+  var values []interface{}
+  values = append(values, 42)
+  values = append(values, "Hello")
+  fmt.Println(values)
+  
 }
