4bb5542f8550fcdcf891d3e29166824dbded477f
davidchenbest <davidchen108@gmail.com>
2023-07-11 22:06:59 -0400

    separate into functions

--- main.go
+++ main.go
@@ -8,6 +8,32 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+func createTable(db *sql.DB) {
+	// Create table
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func insertData(db *sql.DB) {
+	// Insert data
+	_, err := db.Exec("INSERT INTO users (name) VALUES (?)", "John Doe")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func queryData(db *sql.DB) *sql.Rows {
+	// Query data
+	rows, err := db.Query("SELECT id, name FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return rows
+}
+
 func main() {
 	// Open a connection to SQLite database
 	db, err := sql.Open("sqlite3", "example.db")
@@ -16,25 +42,8 @@ func main() {
 	}
 	defer db.Close()
 
-	// Create table
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Insert data
-	_, err = db.Exec("INSERT INTO users (name) VALUES (?)", "John Doe")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Query data
-	rows, err := db.Query("SELECT id, name FROM users")
-	if err != nil {
-		log.Fatal(err)
-	}
+	rows := queryData(db)
 	defer rows.Close()
-
 	// Process query results
 	for rows.Next() {
 		var id int
@@ -45,7 +54,6 @@ func main() {
 		}
 		fmt.Printf("ID: %d, Name: %s\n", id, name)
 	}
-
 	if err = rows.Err(); err != nil {
 		log.Fatal(err)
 	}
