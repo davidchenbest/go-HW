43d09ad6712d0f1c8e39e1264ef236b3848097ea
davidchenbest <davidchen108@gmail.com>
2023-07-11 22:06:42 -0400

    sqlite

--- main.go
+++ main.go
@@ -1,8 +1,52 @@
 package main
 
-import(
+import (
+	"database/sql"
+	"fmt"
+	"log"
 
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func main()  {
+func main() {
+	// Open a connection to SQLite database
+	db, err := sql.Open("sqlite3", "example.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Create table
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Insert data
+	_, err = db.Exec("INSERT INTO users (name) VALUES (?)", "John Doe")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Query data
+	rows, err := db.Query("SELECT id, name FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	// Process query results
+	for rows.Next() {
+		var id int
+		var name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("ID: %d, Name: %s\n", id, name)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
