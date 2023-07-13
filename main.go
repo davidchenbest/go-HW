c123c2fe831e23265ec3ee15b5ec649abf08e3fb
davidchenbest <davidchen108@gmail.com>
2023-07-13 16:17:41 -0400

    add Node of any

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import(
+import "fmt"
 
-)
+type Node[T any] struct {
+	next *Node[T]
+	val  T
+}
 
-func main()  {
+func main() {
+	c := Node[int]{val: 3}
+	b := Node[int]{val: 2, next: &c}
+	a := Node[int]{val: 1, next: &b}
+	// d := Node[int]{val: 1}
+	// e := Node[int]{val: 1}
+
+	fmt.Println(a.next.next)
 }
