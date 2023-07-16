3eff583c6f98e694171871d787c20042cd522994
davidchenbest <davidchen108@gmail.com>
2023-07-15 23:31:02 -0400

    json

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import(
-
+import (
+	"encoding/json"
+	"fmt"
 )
 
-func main()  {
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func main() {
+	jsonData := []byte(`{"name":"Alice", "age":30}`)
+
+	// var person Person
+	var person map[string]any
+	err := json.Unmarshal(jsonData, &person)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	// fmt.Println(person.Name)
+	fmt.Println(person["name"])
 }
