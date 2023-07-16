c3f40865028049520fa15d1d93faa24cfdd8135f
davidchenbest <davidchen108@gmail.com>
2023-07-16 12:06:23 -0400

    rod

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module example.com/hw
 
 go 1.20
+
+require github.com/go-rod/rod v0.113.4
+
+require (
+	github.com/go-rod/stealth v0.4.9 // indirect
+	github.com/ysmood/fetchup v0.2.3 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/got v0.34.1 // indirect
+	github.com/ysmood/gson v0.7.3 // indirect
+	github.com/ysmood/leakless v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import(
+import (
+	"fmt"
+	"time"
 
+	"github.com/go-rod/rod"
+	"github.com/go-rod/stealth"
 )
 
-func main()  {
+func main() {
+	url := "https://stockx.com/sell/air-jordan-1-high-og-spider-man-across-the-spider-verse"
+	browser := rod.New().MustConnect().NoDefaultDevice()
+
+	page := stealth.MustPage(browser)
+	page.MustNavigate(url)
+
+	// page := browser.MustPage(url)
+	// .MustWindowFullscreen()
+
+	// page.MustElement("#searchInput").MustInput("earth")
+	// page.MustElement("#search-form > fieldset > button").MustClick()
+
+	// el := page.MustElement("#mw-content-text > div.mw-parser-output > p:nth-child(7)")
+	// fmt.Println(el.MustText())
+
+	// page.MustWaitStable().MustScreenshot("a.png")
+
+	el := page.MustElement("h1")
+	fmt.Println(el.MustText())
+
+	time.Sleep(time.Hour)
 }
+
+// go run . -rod="show,slow=1s,trace"
