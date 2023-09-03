a52c5a3cf6f3f58e41a4383753d9be59a0809585
davidchenbest <davidchen108@gmail.com>
2023-09-03 16:22:19 -0400

    panic recover

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import(
+import "fmt"
 
-)
+func main() {
+	// Defer a function that recovers from panics
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic:", r)
+		}
+	}()
 
-func main()  {
+	// Simulate a panic
+	divideByZero()
+
+	// This will not be reached due to the panic and recovery
+	fmt.Println("End of the program")
+}
+
+func divideByZero() {
+	panic("not allowed")
 }
